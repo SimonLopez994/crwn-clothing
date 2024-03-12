d963f8dab62755db8e2b25193607b4579a00d788
Simon <chimon994@gmail.com>
2024-03-11 21:40:03 -0300

    created checkout page along with checkout items and checkout cart functionality

---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,28 @@
+import './checkout-item.styles.scss'
+import { CartContext } from '../../contexts/cart.context'
+import { useContext } from 'react'
+const CheckoutItem = ({ cartItem }) => {
+    const { name, imageUrl, price, quantity } = cartItem
+    const { deleteProductFromArray, addItemToCart, removeItem } = useContext(CartContext)
+
+    const deleteProductHandler = () => deleteProductFromArray(cartItem)
+
+    const addItemHandler = () => addItemToCart(cartItem);
+    const removeItemHandler = () => removeItem(cartItem)
+    return (
+        <div className="checkout-item-container">
+            <div className="image-container">
+                <img src={imageUrl} alt={`${name}`} />
+            </div>
+            <span className="name">{name}</span>
+            <span className="quantity">
+                <div className='arrow' onClick={removeItemHandler}>&#10094;</div>
+                <span className='value'>{quantity}</span>
+                <div className='arrow' onClick={addItemHandler}>&#10095;</div>
+            </span>
+            <span className="price">{price}</span>
+            <div className="remove-button" onClick={deleteProductHandler}>&#10005;</div>
+        </div>
+    )
+}
+export default CheckoutItem
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -18,29 +18,68 @@ const addCartItem = (cartItems, productToAdd) => {
     return [...cartItems, { ...productToAdd, quantity: 1 }]
 }
 
+//decrement function
+
+const removeItemFromCart = (cartItems, productToRemove) => {
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id == productToRemove.id
+    );
+
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter((cartItem) => cartItem.id !== productToRemove.id)
+    }
+
+    return cartItems.map(
+        (cartItem) => cartItem.id == productToRemove.id
+            ? { ...existingCartItem, quantity: cartItem.quantity - 1 }
+            : cartItem)
+}
+
+const deleteProduct = (cartItems, productToDelete) => {
+
+    return cartItems.filter((cartItem) => cartItem.id !== productToDelete.id)
+}
+
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => { },
     cartItems: [],
     addItemToCart: () => { },
-    cartCount: 0
+    removeItem: () => { },
+    deleteProductFromArray: () => { },
+    cartCount: 0,
+    cartTotal: 0
 });
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [cartCount, setCartCount] = useState(0);
-    
-    
-    useEffect(() => {
-const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
-setCartCount(newCartCount);
-    }, [cartItems])
+    const [cartTotal, setCartTotal] = useState(0);
 
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd))
     }
-    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount }
+    const removeItem = (productToRemove) => {
+        setCartItems(removeItemFromCart(cartItems, productToRemove)) // Aquí se pasa productToRemove como argumento
+    }
+
+    const deleteProductFromArray = (productToDelete) => {
+        setCartItems(deleteProduct(cartItems, productToDelete))
+    }
+
+    useEffect(() => {
+        const newCartCount = cartItems.reduce((accumulator, currentValue) => accumulator + currentValue.quantity, 0)
+        setCartCount(newCartCount)
+    }, [cartItems])
+
+    useEffect(() => {
+        const newCartTotal = cartItems.reduce((total, currentValue) => total + currentValue.quantity * currentValue.price, 0)
+        setCartTotal(newCartTotal)
+    }, [cartItems])
+
+    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount, removeItem, deleteProductFromArray, cartTotal }
     return (
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
     )
